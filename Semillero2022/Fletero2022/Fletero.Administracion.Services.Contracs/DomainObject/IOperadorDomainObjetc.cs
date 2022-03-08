579502bf00876d6985bd8a7b7dc889d24976b2c2
pedroog304 <pedroog304@gmail.com>
2022-03-08 08:16:37 -0700

    se agrego domain, manager y controler de operador

---
+++ Semillero2022/Fletero2022/Fletero.Administracion.Services.Contracs/DomainObject/IOperadorDomainObjetc.cs
@@ -0,0 +1,16 @@
+﻿using Fletero.Administracion.Services.Contracs.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fletero.Administracion.Services.Contracs.DomainObject
+{
+    public interface IOperadorDomainObject
+    {
+        List<OperadorDTO> ObtenerOperadorList();
+        int Guardar(OperadorDTO operadorDTO);
+        OperadorDTO Editar(OperadorDTO operadorDTO, int idOperador);
+    }
+}
---
+++ Semillero2022/Fletero2022/Fletero.Administracion.Services/DomainObject/OperadorDomainObjetc.cs
@@ -0,0 +1,74 @@
+﻿using Fletero.Administracion.Integration.DAO;
+using Fletero.Administracion.Services.Contracs.DomainObject;
+using Fletero.Administracion.Services.Contracs.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fletero.Administracion.Services.DomainObject
+{
+    public class OperadorDomainObject: IOperadorDomainObject
+    {
+        public List<OperadorDTO> ObtenerOperadorList()
+        {
+            List<OperadorDTO> operadorList = new List<OperadorDTO>();
+
+            try
+            {
+                operadorDAO DAO = new operadorDAO();
+
+                operadorList = DAO.ObtenerOperadorList();
+
+                foreach (var item in operadorList)
+                {
+                    Console.Write(string.Format("idOperador : {0}", item.idOperador));
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return operadorList;
+        }
+        public int Guardar(OperadorDTO operadorDTO)
+        {
+            int idOperador = 0;
+
+            try
+            {
+                operadorDAO DAO = new operadorDAO();
+
+                idOperador = DAO.Guardar(operadorDTO);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return idOperador;
+        }
+        public OperadorDTO Editar(OperadorDTO operadorDTO, int idOperador)
+        {
+            OperadorDTO dto = new OperadorDTO();
+
+            try
+            {
+                operadorDAO DAO = new operadorDAO();
+
+                dto = DAO.Editar(operadorDTO, idOperador);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return dto;
+        }
+    }
+}
